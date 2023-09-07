5bfbdf3b0c44ec1559ff31e585e65cb870629e03
danietakeshi <60260132+danietakeshi@users.noreply.github.com>
2023-09-07 18:50:16 -0300

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
 if [ -d "raw_files" ]; then
-  echo "Folder exists"
+  echo "Folder raw_files exists"
 else
   echo "Creating Folder raw_files..."
   mkdir raw_files
 fi
 
 if [ -d "parquet_files" ]; then
-  echo "Folder exists"
+  echo "Folder parquet_files exists"
 else
   echo "Creating Folder parquet_files..."
   mkdir parquet_files
 fi
 
+if [ -d "database" ]; then
+  echo "Folder database exists"
+else
+  echo "Creating Folder parquet_files..."
+  mkdir database
+fi
+
 cd python
-python dados_abertos_etl.py -s "Qualificacoes"
-python dados_abertos_etl.py -s "Cnaes"
-python dados_abertos_etl.py -s "Naturezas"
-python dados_abertos_etl.py -s "Municipios"
-python dados_abertos_etl.py -s "Paises"
-python dados_abertos_etl.py -s "Motivos"
-python dados_abertos_etl.py -s "Empresas"
-python dados_abertos_etl.py -s "Simples"
-python dados_abertos_etl.py -s "Socios"
-python dados_abertos_etl.py -s "Estabelecimentos"
+python3 dados_abertos_etl.py -s "Qualificacoes"
+python3 dados_abertos_etl.py -s "Cnaes"
+python3 dados_abertos_etl.py -s "Naturezas"
+python3 dados_abertos_etl.py -s "Municipios"
+python3 dados_abertos_etl.py -s "Paises"
+python3 dados_abertos_etl.py -s "Motivos"
+python3 dados_abertos_etl.py -s "Empresas"
+python3 dados_abertos_etl.py -s "Simples"
+python3 dados_abertos_etl.py -s "Socios"
+python3 dados_abertos_etl.py -s "Estabelecimentos"
 
 cd ../dbt_dados_abertos
 dbt run --vars "is_test_run: false"
